8ea14c02f54db09e1a1fa27ee43f128532f332b8
ShiraBrosh <155542654+ShiraBrosh@users.noreply.github.com>
2024-02-12 10:57:35 +0200

    Add files via upload

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -1,73 +1,62 @@
+#include "my_mat.h"
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_WEIGHT 20           // Maximum weight capacity
-#define NUM_ITEMS 5             // Number of items
-#define MAX_NAME_LENGTH 20      // Max length for a product name
-#define TRUE 1
-#define FALSE 0
 
-int knapsack(int weights[MAX_WEIGHT], int values[NUM_ITEMS], int selected[NUM_ITEMS]);
+#define NUM_PRODUCTS 5  // Number of products   
+#define MAX_CAPACITY 20 // Max capacity of bag
 
-int main() {
-    char item_names[NUM_ITEMS][MAX_NAME_LENGTH + 1]; // one more for '\0' 
-    int item_values[NUM_ITEMS] = {0};
-    int item_weights[MAX_WEIGHT] = {0};
+/* Return the maximum of two values */
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
-    for (int i = 0; i < NUM_ITEMS; i++) {
-        scanf("%s %d %d", item_names[i], &item_values[i], &item_weights[i]);
-        if (i < NUM_ITEMS - 1) {
-            scanf(" ");
+/* This function creates the table and enters selected_bool the selected items and returns the max value of all the products */
+int chooseItems(int weights[], int values[], int selected_bool[]) {
+    /* Create the table */
+    int table[NUM_PRODUCTS + 1][MAX_CAPACITY + 1];
+    for (int i = 0; i <= NUM_PRODUCTS; i++) {
+        for (int j = 0; j <= MAX_CAPACITY; j++) {
+            if (i == 0 || j == 0)
+                table[i][j] = 0;
+            else if (weights[i - 1] <= j)
+                table[i][j] = MAX(table[i - 1][j], values[i - 1] + table[i - 1][j - weights[i - 1]]);
+            else
+                table[i][j] = table[i - 1][j];
         }
     }
+    int x = NUM_PRODUCTS;
+    int y = MAX_CAPACITY;
+    while (x > 0 && y > 0) {
+        selected_bool[x - 1] = (table[x][y] == table[x - 1][y]) ? 0 : 1;
+        x -= 1;
+        if (selected_bool[x] == 1)
+            y -= weights[x];
+    }
+    return table[NUM_PRODUCTS][MAX_CAPACITY];
+}
 
-    int selected_items[NUM_ITEMS] = {FALSE}; 
-
-    int max_profit = knapsack(item_weights, item_values, selected_items);
-
-    printf("Maximum profit: %d\n", max_profit);
+int main() {
+    int selected_bool[NUM_PRODUCTS];
+    char products[NUM_PRODUCTS];
+    int values[NUM_PRODUCTS] = {0};
+    int weights[NUM_PRODUCTS] = {0};
+    // Insert input of products, values, and weights
+    for (int i = 0; i < NUM_PRODUCTS; i++) {
+        scanf(" %c", &products[i]);
+        scanf("%d", &values[i]);
+        scanf("%d", &weights[i]);
+    }
+    // print results
+    int maxProfit = chooseItems(weights, values, selected_bool);
+    printf("Maximum profit: %d\n", maxProfit);
     printf("Selected items:");
+    for (int i = 0; i < NUM_PRODUCTS; i++) {
+        if (selected_bool[i] == 1)
+            printf(" %c", products[i]);
+    }
 
-    for (int i = 0; i < NUM_ITEMS; i++) {
-        if (selected_items[i] == TRUE) {                   
-            printf(" %s", item_names[i]);
-        }
+    int result[NUM_PRODUCTS]; // Define result array
+    for (int i = 0; i < NUM_PRODUCTS; ++i) {
+        result[i] = selected_bool[i];
     }
 
     return 0;
-}
-
-int knapsack(int weights[MAX_WEIGHT], int values[NUM_ITEMS], int selected[NUM_ITEMS]) {
-    int matrix[NUM_ITEMS + 1][MAX_WEIGHT + 1]; // one more for line zero
-
-    for (int i = 0; i <= NUM_ITEMS; i++) { // Initialize the matrix with 0
-        for (int j = 0; j <= MAX_WEIGHT; j++) {
-            matrix[i][j] = 0;
-        }
-    }
-
-    for (int i = 1; i <= NUM_ITEMS; i++) { // make the dynamic programming
-        for (int j = 1; j <= MAX_WEIGHT; j++) {
-            if (weights[i - 1] <= j) {
-                if (values[i - 1] + matrix[i - 1][j - weights[i - 1]] > matrix[i - 1][j]) {
-                    matrix[i][j] = values[i - 1] + matrix[i - 1][j - weights[i - 1]];
-                } 
-                else {
-                    matrix[i][j] = matrix[i - 1][j];
-                }
-            } 
-            else {
-                matrix[i][j] = matrix[i - 1][j];
-            }
-        }
-    }
-
-    int total_weight = MAX_WEIGHT;
-    for (int i = NUM_ITEMS; i > 0; i--) {
-        if (matrix[i][total_weight] != matrix[i - 1][total_weight]) {
-            selected[i - 1] = TRUE;
-            total_weight -= weights[i - 1];        
-        }
-    }
-
-    return matrix[NUM_ITEMS][MAX_WEIGHT];
 }
